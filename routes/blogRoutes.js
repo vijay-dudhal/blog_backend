72801ca8e7e8a4fc1c139d18e58107ff079e4b8c
vijay-dudhal <vijaydudhal8010@gmail.com>
2024-11-09 11:04:11 +0530

    add login in route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,11 @@
 const {getblog,postBlog,putBlog,deleteBlog} = require("../controller/blogController")
+const auth = require("../middleware/auth")
 
 const Route = require("express").Router()
 
 Route.get("/",getblog)
-Route.post("/",postBlog)
-Route.put("/:id",putBlog)
-Route.delete("/:id",deleteBlog)
+Route.post("/",auth,postBlog)
+Route.put("/:id",auth,putBlog)
+Route.delete("/:id",auth,deleteBlog)
 
 module.exports = Route
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
-const {getUser,postUser,putUser,deleteUser} = require("../controller/userController")
+const {getUser,postUser,putUser,deleteUser, login} = require("../controller/userController")
 
 const Route = require("express").Router()
 
 Route.get("/",getUser)
 Route.post("/",postUser)
+Route.post("/login",login)
 Route.put("/:id",putUser)
 Route.delete("/:id",deleteUser)
 
